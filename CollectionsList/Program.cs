c9d8d3287a0910714e12325803516b35651cb19e
AndrewKrazhev <accumhelp@ya.ru>
2024-03-18 01:56:54 +0300

    Добавьте файлы проекта.

---
+++ CollectionsList/Program.cs
@@ -0,0 +1,44 @@
+﻿using CollectionsList;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Привет! Это пример, не проходите мимо!");
+        Console.ResetColor();
+        
+        //Создаём список сцен. В качестве типа данных списка выступает объект - класс Scene.
+        List<Scene> listScenes = new List<Scene>();
+        
+        listScenes.Add(new Scene());
+            listScenes[0].SceneActions.AddRange(new[] { "Пойти и ничего не делать", "Пойти поесть" });
+            listScenes[0].SceneGoTo.AddRange(new[] {1, 2});
+        listScenes.Add(new Scene());
+            listScenes[1].SceneActions.AddRange(new[] { "Купить шаурму", "Купить тетрадку" });
+            listScenes[1].SceneGoTo.AddRange(new[] { 0, 2 });
+        listScenes.Add(new Scene());
+            listScenes[2].SceneActions.AddRange(new[] { "Обнять клоуна", "Ударить клоуна" });
+            listScenes[2].SceneGoTo.AddRange(new[] { 777, 777 });
+        
+        //Добавление сцены к списку по одной. Громоздко, не очень понятно.
+        /*Scene FirstScene = new Scene();
+        FirstScene.SceneActions.Add("Упасть и не вставать");
+        FirstScene.SceneActions.AddRange (new[] { "Пойти и ничего не делать", "Пойти поесть" });
+        FirstScene.SceneGoTo.AddRange(new[] { 2, 3, 4 });
+        FirstScene.PrintScene();
+        */
+
+
+        int nextScene;
+        nextScene = listScenes[0].PrintScene();        
+
+        while (nextScene != 777) {
+            int tmp;
+            Console.WriteLine("===============");
+            tmp = listScenes[nextScene].PrintScene();
+            nextScene = tmp;
+        }
+        Console.WriteLine("GAME OVER!!!");
+    }
+}
---
+++ CollectionsList/Scene.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsList
+{
+    public class Scene
+    {
+        List<string> _actions = new List<string>();
+        List<int> _goToScene = new List<int>();
+        ConsoleKeyInfo key;
+        int _gotoMark;
+
+
+        /// <summary>
+        /// Список вариантов действия пользователя.
+        /// </summary>
+        public List<string> SceneActions {            
+            get {return _actions; } 
+            set {_actions = value; }
+        }
+
+        /// <summary>
+        /// Список переходов в зависимости от выбора пользователя.
+        /// </summary>
+        public List<int> SceneGoTo
+        {
+            get { return _goToScene; }
+            set { _goToScene = value; }
+        }
+
+        /// <summary>
+        /// Выводит в консоль сцену.
+        /// </summary>
+        /// <returns>
+        /// Номер следующей сцены.
+        /// </returns>
+        public int PrintScene() 
+        {
+            int i = 0;
+            foreach (var action in SceneActions) 
+            {
+                Console.WriteLine($"{i}.{action}");
+                i++;                
+            }
+
+            key = Console.ReadKey();            
+                        
+            while (key.KeyChar - 48 > _goToScene.Count-1)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Выбор за пределами возможного. Попробуйте снова");
+                key = Console.ReadKey();
+            }
+                Console.WriteLine();
+                return _goToScene[key.KeyChar - 48];
+        }
+
+
+    }
+}
